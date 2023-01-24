fa568a49de337d1b0f5625dcb7ddd791e6e5eb5a
watwatwa <barnach@student.agh.edu.pl>
2023-01-24 15:57:06 +0100

    total_volume

--- wall.py
+++ wall.py
@@ -23,3 +23,15 @@ for w in walls:
             ext_walls.append(w)
             
 print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
+
+
+totalvolume = 0
+
+for w in ext_walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    for psetname, pset_dict in psets.items():
+        for name, value in pset_dict.items():
+            # print (f"{name}: {value}")
+            if name == "NetVolume":
+                totalvolume += float(value)
+print(f'Objętość: {totalvolume:.2f}')
