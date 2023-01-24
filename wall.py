c9fdd7c3c666404288d4e98a7caa6fa1992803af
watwatwa <barnach@student.agh.edu.pl>
2023-01-24 15:55:45 +0100

    ext_walls

--- wall.py
+++ wall.py
@@ -12,3 +12,14 @@ walls = m.by_type("IfcWall")
 wall = walls[1]
 pset_for_wall = ifcopenshell.util.element.get_psets(wall)
 print(pset_for_wall)
+
+
+walls = m.by_type("IfcWall")
+ext_walls = []
+for w in walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    if psets.get("Pset_WallCommon"):
+        if bool(psets.get("Pset_WallCommon").get("IsExternal")):
+            ext_walls.append(w)
+            
+print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
